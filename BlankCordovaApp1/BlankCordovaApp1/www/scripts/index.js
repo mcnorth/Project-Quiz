3f8c194c39bccae44c0d66432e5e522f92edc0d0
DESKTOP-LKCNPUO\kelli <kelliemcnaughton@gmail.com>
2017-07-13 13:09:54 +1000

    Added starter files

--- BlankCordovaApp1/BlankCordovaApp1/www/scripts/index.js
+++ BlankCordovaApp1/BlankCordovaApp1/www/scripts/index.js
@@ -2,29 +2,59 @@
 // http://go.microsoft.com/fwlink/?LinkID=397704
 // To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints, 
 // and then run "window.location.reload()" in the JavaScript Console.
-(function () {
-    "use strict";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
 
-    function onDeviceReady() {
-        // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-    };
+function GetQuestions()
+{
+    var question = document.getElementById("questions");
 
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
-    };
+    //create javascript object
+    var requestURL = "questions.json";
 
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
-} )();
+    //create HttpRequest object
+    var request = new XMLHttpRequest();
+
+    //open the request object
+    request.open("GET", requestURL);
+
+    //let the server know we want a json object
+    request.responseType = "json";
+
+    //send the request
+    request.send();
+
+    request.onload = function ()
+    {
+        //store the response from the server ina variable
+        var quizQuestions = request.response;
+
+        //call a function to fill the page
+        DisplayPage(quizQuestions);
+    }
+
+    function DisplayPage(jsonObject)
+    {
+        //var quest = jsonObject["questions"];
+        //var text = JSON.stringify(quest);
+
+        //create the element
+        var myH1 = document.createElement("h1");
+
+        //modify the text to the value of the json object "id"
+        myH1.textContent = jsonObject["id"];
+
+        //insert the content inside the div
+        question.appendChild(myH1);
+
+        //for (var i = 0; i < text.length; i++)
+        //{
+
+        //    var myH2 = document.createElement("h2");
+        //    myH2.textContent = text[i].id;
+
+        //    question.appendChild(myH2);
+
+        //}
+    }
+
+}
